c1a34c456932652d162ba99cacd8fff16dd077c7
Salvadorvga2004 <116208397+Salvadorvga2004@users.noreply.github.com>
2024-10-18 20:26:36 -0600

    a?

---
+++ client/src/app/services/datos-esp.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Especialistas, Usuarios } from '../models/models';
+import { catchError, Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatosEspService {
+  private Back_Api = 'http://localhost:3002/api';
+
+  constructor(private http:HttpClient) { }
+
+  getEsp(){
+    return this.http.get<Usuarios[]>(`${this.Back_Api}/usuarioEsp`)
+  }
+
+  createUsEsp(body:Usuarios):Observable<any>{
+    return this.http.post(`${this.Back_Api}/usuarioEsp/usuarios`,body).pipe(
+      catchError(error => {
+        console.error('Error al guardar usuario:', error);
+        return of(null);
+      })
+    );
+  }
+
+  createEsp(body:Especialistas):Observable<any>{
+    return this.http.post(`${this.Back_Api}/usuarioEsp/especialistas`,body).pipe(
+      catchError(error => {
+        console.error('Error al guardar especialista:', error);
+        return of(null);
+      })
+    );
+  }
+
+}
