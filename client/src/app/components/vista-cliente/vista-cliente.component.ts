babb7c83c988614d7b6bf19ee11b21076302a839
Alejandrox666 <116208108+Alejandrox666@users.noreply.github.com>
2024-10-21 20:12:52 -0600

    Se agrego la API de Youtube

--- client/src/app/components/vista-cliente/vista-cliente.component.ts
+++ client/src/app/components/vista-cliente/vista-cliente.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DatosEspService } from 'src/app/services/datos-esp.service';// Ajusta la ruta según sea necesario
 import { Usuarios,Especialistas } from 'src/app/models/models';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { YoutubeService } from 'src/app/services/youtube.service';
 
 @Component({
   selector: 'app-vista-cliente',
@@ -8,18 +10,40 @@ import { Usuarios,Especialistas } from 'src/app/models/models';
   styleUrls: ['./vista-cliente.component.css']
 })
 export class VistaClienteComponent implements OnInit {
+
+  videos: any[] | undefined; // Almacena los datos de los videos
+  videoUrl: SafeResourceUrl; 
+
   especialistas: (Usuarios & Especialistas)[] = [];
 
-  constructor(private datosEspService: DatosEspService) { }
+  constructor(private datosEspService: DatosEspService, private youtubeService: YoutubeService, private sanitizer: DomSanitizer) { 
+    this.videoUrl = '';
+  }
 
   ngOnInit(): void {
     this.getEspecialistas();
+     // Llama al servicio para obtener los videos
+     this.youtubeService.getVideos().subscribe((data: any) => {
+      this.videos = data.items;
+    });
+
   }
 
+  
   getEspecialistas(): void {
     this.datosEspService.getEsp().subscribe(
       (      data: any[]) => this.especialistas = data,
       (      error: any) => console.error('Error al obtener especialistas:', error)
     );
   }
+
+  // Esta función crea una URL segura para el video
+  getVideoUrl(videoId: string): SafeResourceUrl {
+    if (videoId) {
+      const url = `https://www.youtube.com/embed/${videoId}`;
+      return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+    }
+    return '';
+  }
+
 }
---
+++ client/src/app/services/youtube.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class YoutubeService {
+  private apiKey = 'AIzaSyBuEsieZr1Pm4k4xAXyqb7mRQa6tS6FVlI';
+  private apiUrl = 'https://www.googleapis.com/youtube/v3';
+
+  constructor(private http: HttpClient) { }
+
+  getVideos(): Observable<any> {
+    const url = `${this.apiUrl}/search?key=${this.apiKey}&part=snippet&type=video&q=Salud`;
+    return this.http.get(url);
+  }
+}
