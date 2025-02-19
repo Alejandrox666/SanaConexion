7925d5cef138349e8569187ef422036b8affea69
Salvadorvga2004 <116208397+Salvadorvga2004@users.noreply.github.com>
2025-02-19 15:12:29 -0600

    Redes Sociales

--- client/src/app/components/politicas-priv/politicas-priv.component.ts
+++ client/src/app/components/politicas-priv/politicas-priv.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ClientesService } from 'src/app/services/clientes.service';
 
 @Component({
   selector: 'app-politicas-priv',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./politicas-priv.component.css']
 })
 export class PoliticasPrivComponent {
+  usuarioTemporal: any;
+
+  constructor(private clienteService: ClientesService, private router: Router) { }
+  ngOnInit(): void {
+    const usuario = this.clienteService.getUsuarioTemporal2();
+    if (usuario && usuario.tipoUsuario) {
+      this.usuarioTemporal = usuario.tipoUsuario;
+
+    }
+  }
+
+  irAlInicio(): void {
+    let ruta = ''
+
+    if (this.usuarioTemporal === 'Cliente') {
+      ruta = '/vistaClient';
+    } else if (this.usuarioTemporal === 'Especialista') {
+      ruta = '/inicioE';
+    }
+
+    this.router.navigate([ruta]);
+  }
 
 }
