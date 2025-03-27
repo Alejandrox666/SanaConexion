9a3f6af2c910e75d4b292c6220718517ac854912
Alejandrox666 <116208108+Alejandrox666@users.noreply.github.com>
2025-03-26 21:05:59 -0600

    Act 4

--- server/src/keys.ts
+++ server/src/keys.ts
@@ -3,7 +3,7 @@ export default {
     database: {
         host: 'byxkfgn533c0gq7wr3zp-mysql.services.clever-cloud.com',
         user: 'ubqrdo4zqgwd7wrj',
-        password:'149os43p0tlkzbTvQ4DEzoY2',
+        password:'os43p0tlkzbTvQ4DEzoY',
         database: 'byxkfgn533c0gq7wr3zp'
     }
 
